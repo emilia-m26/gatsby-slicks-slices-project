1424990de3744517612bf8fce23004e63ad8fab9
Emilia <emilia.mena26@gmail.com>
2020-11-18 16:47:13 -0500

    basic 404 page

--- gatsby/.cache/sync-requires.js
+++ gatsby/.cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/emiliamena/Documents/coding/gatsby-slicks-slices-project/gatsby/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/emiliamena/Documents/coding/gatsby-slicks-slices-project/gatsby/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/emiliamena/Documents/coding/gatsby-slicks-slices-project/gatsby/src/pages/index.js")))
 }
 
