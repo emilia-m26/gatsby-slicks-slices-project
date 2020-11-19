34155df03606b56474406923202c18df34ce89d0
Emilia <emilia.mena26@gmail.com>
2020-11-19 13:18:01 -0500

    tiltinh nav bar links

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -15,6 +15,20 @@ const NavStyles = styled.nav`
         text-line: center;
         list-style: none;
     }
+    li {
+        --rotate: -2deg;
+        transform: rotate(var(--rotate));
+        order: 1;
+        &:nth-child(1) {
+            --rotate: 1deg;
+        }
+        &:nth-child(2) {
+            --rotate: -2.5deg;
+        }
+        &:nth-child(4) {
+            --rotate: 2.5deg;
+        }
+    }
 `;
 
 export default function Nav() {
