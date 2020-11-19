f31c00c8c3e81af113d6591765bb35e1affbd990
Emilia <emilia.mena26@gmail.com>
2020-11-19 13:45:28 -0500

    layout js edits

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+import 'normalize.css'; //package from package.json
 import Nav from './Nav';
 import Footer from './Footer';
-import 'normalize.css'; //package from package.json
+
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
 
+
 const ContentStyles = styled.div`
-    
+    background: white;
+    padding: 2rem;
 `;
 
 export default function Layout({ children }) {
  return (
-     <div>
+     <>
          <GlobalStyles />
          <Typography />
-         <Nav />
-         {children}
-         <Footer />
-     </div>
+         <ContentStyles>
+            <Nav />
+            {children}
+            <Footer />
+         </ContentStyles>
+     </>
  )
 };
