e6c4c41d6cc7f8c40b63315b70bf3cc6ae83c7e5
Emilia <emilia.mena26@gmail.com>
2020-11-19 13:07:10 -0500

    nav styling

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+//only will apply to this component - scoped styles
+const NavStyles = styled.nav`
+    margin-bottom: 3rem;
+    ul {
+        margin: 0;
+        padding: 0;
+        display: grid;
+        grid-template-columns: 1fr 1fr auto 1fr 1fr;
+        grid-gap: 2rem;
+        align-items: center;
+        text-line: center;
+        list-style: none;
+    }
+`;
 
 export default function Nav() {
     return ( 
-        <nav>
+        <NavStyles>
             <ul>
                 <li><Link to="/">Hot Now</Link></li>
                 <li><Link to="/pizzas/">Pizza Menu</Link></li>
@@ -11,6 +27,6 @@ export default function Nav() {
                 <li><Link to="/slicemasters">SliceMasters</Link></li>
                 <li><Link to="/order">Order Ahead</Link></li>
             </ul>
-        </nav>
+        </NavStyles>
     );
 }
