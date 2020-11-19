b2f726d29a477d7bcfada3f36a0e26a95d7a736e
Emilia <emilia.mena26@gmail.com>
2020-11-19 12:19:46 -0500

    global styles component imported to layout comp

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
 import 'normalize.css'; //package from package.json
+import GlobalStyles from '../styles/GlobalStyles';
+
 
 export default function Layout({ children }) {
  return (
      <div>
+         <GlobalStyles />
          <Nav />
          {children}
          <Footer />
