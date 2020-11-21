44d972217feccbd7ce621f85833d12a063c5ce2d
Emilia <emilia.mena26@gmail.com>
2020-11-21 11:07:51 -0500

    creating data types for sanity backend

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -14,6 +14,31 @@ export default {
             title: 'Pizza Name',
             type: 'string',
             description: 'Name of the pizza'
-        }
+        },
+        {
+            name: 'slug',
+            title: 'slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 100,
+            }
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number',
+            description: 'Price of the pizza in cents',
+            validation: Rule => Rule.min(1000),
+            //TODO - add custom component
+        },
     ]
 };
