af265523573a9d6f4c989bcf8c5c33ba1453cd5c
Emilia <emilia.mena26@gmail.com>
2020-11-21 11:52:18 -0500

    custom preview in sanity for pizza

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -47,5 +47,26 @@ export default {
             type: 'array',
             of: [{ type: 'reference', to: [{type: 'topping'}]}],
         }
-    ]
+    ],
+    preview: {
+        select: {
+           title: 'name',
+            media: 'image',
+            topping0: 'toppings.0.name',
+            topping1: 'toppings.1.name',
+            topping2: 'toppings.2.name',
+            topping3: 'toppings.3.name',
+        },
+        //destructure
+        prepare: ({title, media, ...toppings}) => {
+        //filter undefined toppings out
+        const tops = Object.values(toppings).filter(Boolean)
+        //return preview obj for pizza
+        return {
+            title,
+            media,
+            subtitle: Object.values(tops).join(', '),
+        }
+    }
+    }
 };
