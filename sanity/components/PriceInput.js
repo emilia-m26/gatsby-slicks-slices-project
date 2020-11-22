01217e3463535a0b6fec4c4f25f678724a997712
Emilia <emilia.mena26@gmail.com>
2020-11-22 10:01:50 -0500

    input type for price custom compunent

--- sanity/components/PriceInput.js
+++ sanity/components/PriceInput.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event';
 
-export default function PriceInput() {
+function createPatchFrom(value) {
+    return PatchEvent.from(value === '' ? unset() : set(Number(value)));
+}
+
+//destructuring props
+export default function PriceInput({ type, value, onChange, inputComponent }) {
     return (
-    <div><h2>The Price</h2></div>
+    <div>
+        <h2>{type.title}</h2>
+        <p>{type.description}</p>
+        <input type={type.name} value={value} onChange={event => onChange(createPatchFrom(event.target.value))} ref={inputComponent}/>
+    </div>
     );
+}
+
+//exposing focus method for sanity to run
+PriceInput.focus = function() {
+    this._inputElement.focus();
 }
