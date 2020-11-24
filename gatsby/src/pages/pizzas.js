cf5a313240e22b5a96f661769b6b19dcff7402ca
Emilia <emilia.mena26@gmail.com>
2020-11-24 17:58:13 -0500

    working on pizza query

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+
 
 
 export default function PizzasPage() {
@@ -8,3 +10,29 @@ export default function PizzasPage() {
         </>
     );
 }
+
+//specify page query - gatsby will grab data and pass it to page comp
+export const query = graphql`
+    query PizzaQuery {
+        pizzas: allSanityPizza {
+            nodes {
+                id
+                name
+                slug {
+                    current
+                }
+                toppings {
+                    id
+                    name
+                }
+                image{
+                    asset {
+                        fluid(maxWidth: 400) {
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
