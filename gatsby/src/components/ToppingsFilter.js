04c4dcc500a07ce676af8f6ae98b116e676e07b7
Emilia <emilia.mena26@gmail.com>
2020-11-27 12:20:46 -0500

    counting toppings and sorting by toppings

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import {useStaticQuery, graphql} from 'gatsby';
 
+function countPizzasInToppings(pizzas) {
+    //return pizzas with counts
+    // flat() - take an arrays of arrays and turn it into one big array
+    const counts = pizzas.map(pizza => pizza.toppings).flat().reduce((acc, topping) => {
+        //check if existing topping
+        const existingTopping = acc[topping.id];
+        if(existingTopping){
+            //if is, increment
+            existingTopping.count += 1;
+        } else {
+        //otherwise create new entry and set to 1
+        acc[topping.id] = {
+            id: topping.id,
+            name: topping.name,
+            count: 1,
+            }
+        }
+        return acc;
+    }, {});
+    //sort them based on count
+    const sortedToppings = Object.values(counts).sort((a, b) => b.count - a.count);
+    return sortedToppings;
+}
+
 export default function ToppingsFilter() {
     //get list of all toppings
-    const toppings = useStaticQuery(graphql`
+    //get list of all pizzas with their toppings
+    const {toppings, pizzas} = useStaticQuery(graphql`
         query {
             toppings: allSanityTopping {
                 nodes {
@@ -12,12 +37,21 @@ export default function ToppingsFilter() {
                     vegetarian 
                 }
             }
+            pizzas: allSanityPizza {
+                nodes {
+                    toppings {
+                        name
+                        id
+                    }
+                }
+            }
         }
     `);
-    console.log(toppings);
-    //get list of all pizzas with their toppings
+    
     //count how many pizzas are in each topping
-    //loop ove rlist of toppings and display topping and count of toppings
+    const toppingsWithCounts = countPizzasInToppings(pizzas.nodes);
+    console.log(toppingsWithCounts);
+    //loop over list of toppings and display topping and count of toppings
     ///link it up
         
     return (
