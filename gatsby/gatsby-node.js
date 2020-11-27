50fb8e7a943f158a750b7ce99fa6e07d853a24b4
Emilia <emilia.mena26@gmail.com>
2020-11-27 13:23:25 -0500

    working on gatsby node file to dynamically create pages

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,14 @@
+import ToppingsFilter from "./src/components/ToppingsFilter";
+
+async function turnPizzasIntoPages(params) {
+    //get template for page
+    //query all pizzas
+    //loop over each pizza and create page for that pizza
+};
+
+export async function createPages(params){
+    //create pages dynamically
+    //pizzas
+    //toppings
+    //slicemasters
+}
