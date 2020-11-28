327f1cbcdee5f09e39d3423a5dc1aad201cd8043
Emilia <emilia.mena26@gmail.com>
2020-11-28 10:58:57 -0500

    slicemasters page, rendering - next to style

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
 
 
 export default function SlicemastersPage({ data }) {
@@ -8,7 +9,21 @@ export default function SlicemastersPage({ data }) {
 
     return ( 
         <>
-            <p> hey, I am the slice masters page</p>
+        <div>
+        {slicemasters.map((person) => (
+            <div>
+                <Link to={`slicemaster/${person.slug.current}`}>
+                    <h2>
+                        <span className="mark">
+                            {person.name}
+                        </span>
+                    </h2>
+                </Link>
+                <Img fluid={person.image.asset.fluid}/>
+                <p className="description">{person.description}</p>
+            </div>
+        ))}
+        </div>
         </>
     );
 }
