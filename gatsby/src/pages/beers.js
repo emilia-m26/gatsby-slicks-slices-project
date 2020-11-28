bbe31d365ba42b766c77e0479018ba220d14f836
Emilia <emilia.mena26@gmail.com>
2020-11-28 09:42:35 -0500

    working on beers page query

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 
 export default function BeersPage() {
@@ -8,3 +9,20 @@ export default function BeersPage() {
         </>
     );
 }
+
+export const query = graphql`
+    query {
+        allBeer {
+            nodes {
+                id
+                name
+                price
+                image
+                rating {
+                    average
+                    reviews
+                }
+            }
+        }
+    }
+`;
