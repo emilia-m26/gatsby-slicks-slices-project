64a8d901b8198c1cd9ce1f871f6322b685d4a3d2
Emilia <emilia.mena26@gmail.com>
2020-11-28 09:59:56 -0500

    working on beers page, displaying data on page, ratings, image, price, etc

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -2,17 +2,36 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 
-export default function BeersPage() {
+export default function BeersPage({ data }) {
     return ( 
         <>
-            <p> hey, I am the Beers page</p>
+            <h2 className="center"> We have ${data.beers.nodes.length} Beers available. Dine in Only!</h2>
+            <div>
+                {data.beers.nodes.map(beer => {
+                    const rating =Math.round(beer.rating.average)
+                    return (
+                        <div key={beer.id}>
+                            <img src={beer.image} alt={beer.name} />
+                            <h3>{beer.name}</h3>
+                            {beer.price}
+                            <p title={`${rating} out of 5 stars`}>
+                            {`🌟`.repeat(rating)}
+                            <span style={{filter: `grayscale(100%)`}}>
+                            {`🌟`.repeat(5-rating)}
+                            </span>
+                            <span>({beer.rating.reviews})</span>
+                            </p>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     );
 }
 
 export const query = graphql`
     query {
-        allBeer {
+        beers: allBeer {
             nodes {
                 id
                 name
