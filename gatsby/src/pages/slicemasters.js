2a5450edd73400b5825a2bb86e2527d785443fc6
Emilia <emilia.mena26@gmail.com>
2020-11-28 10:45:42 -0500

    slicemasters data showing on console log, next to display and style

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 
-export default function SlicemastersPage() {
+export default function SlicemastersPage({ data }) {
+    const slicemasters = data.slicemasters.nodes;
+    console.log(slicemasters);
+
     return ( 
         <>
             <p> hey, I am the slice masters page</p>
         </>
     );
 }
+
+export const query = graphql`
+query {
+    slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+            name
+            id
+            slug {
+                current
+            }
+            description
+            image {
+                asset {
+                    fluid(maxWidth: 410) {
+                    ...GatsbySanityImageFluid
+                    }
+                }
+            }
+        }
+    }
+}
+`;
