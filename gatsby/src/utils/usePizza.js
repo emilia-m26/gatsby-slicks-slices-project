783d6e0a0e47026dc083978cd69181cb7fe68a6b
Emilia <emilia.mena26@gmail.com>
2020-12-03 12:58:47 -0500

    starting function usePizza

---
+++ gatsby/src/utils/usePizza.js
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+export default function usePizza({pizzas, inputs}) {
+    //create state to hold order
+    const [order, setOrder] = useState([]);
+    //make function to add things to order
+    //function to remove things from order
+    //send data to serverless function when checking out
+}
