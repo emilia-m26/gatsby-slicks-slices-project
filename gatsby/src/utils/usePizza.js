3a8f7f730696270cf1acd164aff745ebf927af8e
Emilia <emilia.mena26@gmail.com>
2020-12-03 13:03:05 -0500

    adding and removing

--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -4,6 +4,20 @@ export default function usePizza({pizzas, inputs}) {
     //create state to hold order
     const [order, setOrder] = useState([]);
     //make function to add things to order
+    function addToOrder(orderedPizza) {
+        setOrder([...order, orderedPizza]);
+    }
     //function to remove things from order
+    function removeFromOrder(index) {
+        setOrder([
+            //everything before item we want to remove
+            ...order.slice(0, index),
+            //everything after item we want to remove
+            ...order.slice(index + 1) //omit 2nd arg - goes to end
+        ]);
+    }
     //send data to serverless function when checking out
+    return {
+        
+    }
 }
