24f6ca1f41b416ad81537dc6e14a7ff51e8b462e
Emilia <emilia.mena26@gmail.com>
2020-12-03 14:24:23 -0500

    info displaying and price formatted

--- gatsby/src/components/PizzaOrder.js
+++ gatsby/src/components/PizzaOrder.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import MenuItemStyles from '../styles/MenuItemStyles';
+import Img from 'gatsby-image';
+import calculatePizzaPrice from '../utils/calculatePizzaPrice';
+import formatMoney from '../utils/formatMoney';
+
+
 
 export default function PizzaOrder({ order, pizzas, removeFromOrder }){
     return (
         <>
-        {order.map((singleOrder, Index) =>{
+        {order.map((singleOrder, index) =>{
+            const pizza = pizzas.find((pizza) => pizza.id === singleOrder.id);
+
             return (
-                <MenuItemStyles>
-                {singleOrder.id}
+                <MenuItemStyles key={singleOrder.id} >
+                    <Img fluid={pizza.image.asset.fluid} />
+                <h2>{singleOrder.id}</h2>
+                <p>
+                    {formatMoney(calculatePizzaPrice(pizza.price, singleOrder.size))}
+                </p>
                 </MenuItemStyles>
             )
         })}
