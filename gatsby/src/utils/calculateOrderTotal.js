6799253e8adea930c30ff140ab1534f4542bbab7
Emilia <emilia.mena26@gmail.com>
2020-12-04 16:19:55 -0500

    fixing comments

--- gatsby/src/utils/calculateOrderTotal.js
+++ gatsby/src/utils/calculateOrderTotal.js
@@ -4,10 +4,11 @@ import formatMoney from './formatMoney';
 export default function calculateOrderTotal(order, pizzas) {
     //loop over items in order
     const total = order.reduce((runningTotal, singleOrder) =>{
-        const pizza = pizzas.find(pizza => pizza.id === singleOrder.id);
+        const pizza = pizzas.find((singlePizza)=> singlePizza.id === singleOrder.id);
+        //add total to running total
         return runningTotal + calculatePizzaPrice(pizza.price, singleOrder.size);
     }, 0);
     return formatMoney(total);
-    //calc the total for that pizza
-    //add total to running total
+    
+    
 }
