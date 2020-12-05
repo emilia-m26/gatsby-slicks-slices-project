3700e8da64c93b980e26e2cff4d78b706becbbf4
Emilia <emilia.mena26@gmail.com>
2020-12-05 08:49:14 -0500

    adding placeOrder function file, installed nodemailer as dependency in its own json pkg

---
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -0,0 +1,6 @@
+//require nodemailer
+const nodemailer = require('nodemailer');
+//create transport for nodemailer
+const transporter = nodemailer.createTransport({
+    //all authentication variables you need in order to connect to it
+});
