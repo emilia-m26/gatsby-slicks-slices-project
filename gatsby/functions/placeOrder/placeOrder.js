e4398240a24e16a84e6aed4495dc16c669d6c668
Emilia <emilia.mena26@gmail.com>
2020-12-05 09:02:50 -0500

    placeOrder showing email in etheral.email mailbox

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,6 +1,27 @@
 //require nodemailer
 const nodemailer = require('nodemailer');
-//create transport for nodemailer
+//create transport for nodemailer - used etheral.email on this
 const transporter = nodemailer.createTransport({
-    //all authentication variables you need in order to connect to it
+    host: process.env.MAIL_HOST,
+    port: 587,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    }
 });
+
+
+
+exports.handler = async (event, context) => {
+//test send an email
+    const info = await transporter.sendMail({
+        from: "Slick's Slices <slick@example.com>",
+        to: "orders@example.com",
+        subject: "New Order!",
+        html: `<p>Your new pizza order is here</p>`
+    });
+    return {
+        statusCode: 200,
+        body: JSON.stringify(info)
+    }
+}
