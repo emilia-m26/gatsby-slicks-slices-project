dc213907f358df19a212d9190823ccd3a0462ea4
Emilia <emilia.mena26@gmail.com>
2020-12-07 12:57:32 -0500

    starting to work on settings page schema

---
+++ sanity/schemas/storeSettings.js
@@ -0,0 +1,14 @@
+import { MdStore as icon } from 'react-icons/md';
+
+export default {
+    //computer name
+    name: 'storeSettings',
+    //visible title
+    title: 'Settings',
+    type: 'document',
+    //can give icon any React comp
+    icon: icon, 
+    fields: [
+        
+    ],    
+};
