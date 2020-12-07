bb0dd318d55ea04737e533c8882e54c90ee87ed8
Emilia <emilia.mena26@gmail.com>
2020-12-07 13:33:47 -0500

    sidebar.js completed for sanity to show custom settings page to later pull data into front end

--- sanity/sidebar.js
+++ sanity/sidebar.js
@@ -7,5 +7,15 @@ export default function Sidebar() {
         //create new subitem
         S.listItem()
         .title('Home Page')
+        .icon(() => <strong>🍕</strong>)
+        .child(
+            S.editor().schemaType('storeSettings')
+            //make new doc id so we don't have random string of numbers
+            .documentId('downtown') 
+        ),
+        //add in the rest of doc items
+        ...S.documentTypeListItems()
+        //will filter out settings to our custom settings show only
+        .filter(item => item.getId() !== 'storeSettings'),
     ])
 }
