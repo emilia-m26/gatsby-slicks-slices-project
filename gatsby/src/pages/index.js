852545c309190abfd2bc7e563b4b579f9125b9cd
Emilia <emilia.mena26@gmail.com>
2020-12-08 16:40:59 -0500

    adding components needed for homepage

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
+function CurrentlySlicing() {
+    return (
+        <div>
+            <p>Currently Slicing</p>
+        </div>
+    )
+}
+
+function HotSlices() {
+    return (
+        <div>
+            <p>Hot Slices</p>
+        </div>
+    )
+}
+
 export default function HomePage() {
     return ( 
         <div className="center">
             <h1>The Best Pizza Downtown!</h1>
-            <p>Open 11am to 11pm</p>
+            <p>Open 11am to 11pm Every Day</p>
+            <div>
+                <CurrentlySlicing />
+                <HotSlices />
+            </div>
 
         </div>
     );
