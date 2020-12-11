f3c4dc0b8fe3b91fcff2c58e2a11607f77535873
Emilia <emilia.mena26@gmail.com>
2020-12-11 17:10:34 -0500

    mapping over count to show that many loading items on homepage

--- gatsby/src/components/LoadingGrid.js
+++ gatsby/src/components/LoadingGrid.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import { ItemsGrid } from "../styles/Grids";
 
 export default function LoadingGrid({ count }) {
     return (
         <ItemsGrid>
-            
+            {Array.from({ length: count }, (_, i) => (
+             <p>Loading...</p>   
+            ))}
         </ItemsGrid>
     )
 }
