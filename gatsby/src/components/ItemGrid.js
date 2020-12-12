b964ed92903b8e627de282ac818a877ee4c4e9c2
Emilia <emilia.mena26@gmail.com>
2020-12-12 12:11:04 -0500

    all data showing and styling added

--- gatsby/src/components/ItemGrid.js
+++ gatsby/src/components/ItemGrid.js
@@ -9,7 +9,10 @@ export default function ItemGrid({ items }) {
                    <p>
                        <span className="mark">{item.name}</span>
                    </p>
-                   <img src={`${item.image.asset.url}`} alt={item.name} />
+                   <img width="500" height="400" src={`${item.image.asset.url}?w=500&h=400&fit=crop`} alt={item.name} style={{
+                       background: `url(${item.image.asset.metadata.lqip})`,
+                       backgroundSize: 'cover',
+                   }}/>
                </ItemStyles>
            ))}
         </ItemsGrid>
